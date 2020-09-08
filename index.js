dbb713eb24747929435a66ce24cb52ad128abeae
Erick Gonzalez <erick@Ericks-MacBook-Pro.local>
2020-09-08 17:09:23 -0400

    Completed a portion of Rock, Paper, Scissors

--- index.js
+++ index.js
@@ -2,36 +2,57 @@
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
 const votingAge = 18
+var age = 19
 
-if (votingAge > 18) {
-    console.log(votingAge);
+if (age >= votingAge) {
+    console.log("True");
 }
 
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
+var currentGirlfriend = "Katie"
 
+var newGirlfriend = "Delphine"
+
+if (currentGirlfriend !== newGirlfriend) {
+    currentGirlfriend = newGirlfriend;
+    console.log(currentGirlfriend);
+}
 
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
 
+var year = "1999"
+
+var convertedYear = Number(year)
 
 
+console.log(convertedYear)
+console.log(typeof convertedYear)
 
 //Task d: Write a function to multiply a*b 
 
+function multiply(a, b) {
+    result = a * b;
+    return result;
+}
 
-
-
+multiply(5, 2)
 
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
+function humanYearsToDogYears(humanYears) {
+    convertedDogYears = humanYears * 7;
+    return convertedDogYears;
+}
 
+humanYearsToDogYears(40)
 
 
 
@@ -53,19 +74,73 @@ if (votingAge > 18) {
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
   
+function poundsToFeed(weight, age) {
 
+    if (age >= (2/12) && age <= (4/12)) {
+        console.log((weight*.10));
+    } else if (age > (4/12) && age <= (7/12)) {
+        console.log((weight*.05));
+    } else if (age >= (7/12) && age < (1)) {
+        console.log(weight*.04);
+    } else if (age === (4/12) && age === (4/12)) {
+        console.log((weight*.05));
+    } else if (age === (4/12)) {
+        console.log(weight*.04);
+    } else if (age === (4/12) && age === (4/12)) {
+        console.log((weight*.05));
+    } else if (age >= 1){
+        if (0 <= weight && weight <= 5) {
+            console.log(weight*.05);
+        } else if (weight >= 6 && weight <= 10) {
+            console.log(weight*.04);
+        } else if (weight >= 11 && weight <= 15) {
+            console.log(weight*.03);
+        } else {
+            console.log(weight*.02);
+        }
+    }
+ 
+}
+
+poundsToFeed(15, 1)
 
 
 
 /************************************************************** Task 4 **************************************************************/
-// Rock, Paper, Sissors
-// Your function should take a string (either rock paper or sissors)
+// Rock, Paper, Scissors
+// Your function should take a string (either rock paper or scissors)
 // it should return you won or you lost based on the rules of the game (you may need to look up the rules if you have not played before)
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
-  
+const weapons = ["rock", "paper", "scissors"]
+
+var computerWeapon = Math.floor(Math.random() * 2 ) + 1;
+
+
+function rockPaperScissors(myWeapon) {
+
+    if (computerWeapon == "rock") {
+        if myWeapon == "rock" {
+            console.log("It's a draw.");
+        } else if myWeapon == "paper" {
+            console.log("You win!");
+        } else {
+            console.log("You lose!");
+        }
+
+    else if (computerWeapon == "scissors") {
+        if (myWeapon == "scissors") {
+            console.log("It's a draw.");
+        } else if (myWeapon == "rock") {
+            console.log("You win!")
+        } else {
+            console.log("You lose!")
+        }
+    }
+    }
+
+}
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
